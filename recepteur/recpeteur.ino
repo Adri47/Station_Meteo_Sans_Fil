2cd20b0cfa36141517896f968bf834e35128786f
Verboom30 <clement.charles.savary@gmail.com>
2021-10-28 14:14:39 +0200

    ajout recepteur

---
+++ recepteur/recpeteur.ino
@@ -0,0 +1,18 @@
+
+
+void setup()
+{           
+    Serial.begin(9600);                     
+}
+ 
+ 
+void loop()
+{
+  while (Serial.available()>0){
+    //Read the incoming byte
+    Serial.print(Serial.read);
+
+    {
+   
+  
+}
